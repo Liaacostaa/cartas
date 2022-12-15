36ff762210262755aa6e37501d581673109a584c
lacostas <liaacosol@alu.edu.gva.es>
2022-12-15 21:20:25 +0100

    Cartitass

--- src/main/java/es/ieslavereda/cartas/CartasTester.java
+++ src/main/java/es/ieslavereda/cartas/CartasTester.java
@@ -4,6 +4,6 @@ public class CartasTester {
     public static void main(String[] args) {
         Baraja baraja = new Baraja();
         System.out.println(baraja.toString());
-        l
+
     }
 }
